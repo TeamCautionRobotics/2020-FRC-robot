4733402e8bb92c41a77326b0b663dc094fe0390c
Belany <beljzhang@gmail.com>
2020-01-20 20:32:23 -0700

    added colorSensor variable

--- src/main/java/frc/robot/RainbowDJ.java
+++ src/main/java/frc/robot/RainbowDJ.java
@@ -6,20 +6,29 @@
 
 package frc.robot;
 
+import com.revrobotics.ColorSensorV3;
+
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
 
 public class RainbowDJ {
 
     // Encoder declarations
     private final Encoder rainbowDJEncoder;
 
+    //Color Sensor
+    private final ColorSensorV3 colorSensorV3;
+
    // rainbowDJSpinner
 
     // Class initializer
-    public RainbowDJ(int rainbowDJEncoderChannelA, int rainbowDJEncoderChannelB) {
+    public RainbowDJ(int rainbowDJEncoderChannelA, int rainbowDJEncoderChannelB, I2C.Port colorSensorPort) {
     
         // Encoders
         rainbowDJEncoder = new Encoder(rainbowDJEncoderChannelA, rainbowDJEncoderChannelB);
+        // Color Sensor
+        colorSensorV3 = new ColorSensorV3(colorSensorPort);
 
     }
 

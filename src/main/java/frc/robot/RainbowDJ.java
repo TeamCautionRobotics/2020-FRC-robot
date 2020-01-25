c793fd02f54ada18e2a75d1e4383b88b7e3be058
Belany <beljzhang@gmail.com>
2020-01-25 10:47:22 -0700

    added motor and changed method names

--- src/main/java/frc/robot/RainbowDJ.java
+++ src/main/java/frc/robot/RainbowDJ.java
@@ -1,7 +1,8 @@
 /*
     This class includes:
     -rainbowDJEncoder
-
+    -rainbowDJColorSensor
+    -rainbotDJMotor
 */
 
 package frc.robot;
@@ -10,6 +11,7 @@ import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class RainbowDJ {
@@ -17,30 +19,37 @@ public class RainbowDJ {
     // Encoder declarations
     private final Encoder rainbowDJEncoder;
 
-    //Color Sensor
-    private final ColorSensorV3 colorSensorV3;
+    // Color Sensor
+    private final ColorSensorV3 rainbowDJColorSensor;
 
-   // rainbowDJSpinner
+    // ESCs
+    private final VictorSP rainbowDJMotor;
 
     // Class initializer
-    public RainbowDJ(int rainbowDJEncoderChannelA, int rainbowDJEncoderChannelB, I2C.Port colorSensorPort) {
+    public RainbowDJ(int rainbowDJMotorPort, int rainbowDJEncoderChannelA, int rainbowDJEncoderChannelB, I2C.Port colorSensorPort) {
     
+        // ESCs
+        rainbowDJMotor = new VictorSP(rainbowDJMotorPort);
+
         // Encoders
         rainbowDJEncoder = new Encoder(rainbowDJEncoderChannelA, rainbowDJEncoderChannelB);
-        // Color Sensor
-        colorSensorV3 = new ColorSensorV3(colorSensorPort);
 
+        // Color Sensor
+        rainbowDJColorSensor = new ColorSensorV3(colorSensorPort);
     }
 
     // Setters
+    // Positive power moves control panel clockwise
+    public void moveRainbowDJMotor(double power) {
+        rainbowDJMotor.set(power);
+    }
 
     // Getters
-
-    public double getRainbowDJEncoderDistance() {
+    public double getDistance() {
         return rainbowDJEncoder.getDistance();
     }
 
-    public double getRainbowDJEncoderRate() {
+    public double getRate() {
         return rainbowDJEncoder.getRate();
     }
 

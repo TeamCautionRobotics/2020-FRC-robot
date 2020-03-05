44a20500e9a129eff13fee75693b8b1515ccb25b
Sean <seantierney42@gmail.com>
2020-03-04 19:57:04 -0700

    Add arm lock piston

--- src/main/java/frc/robot/Climb.java
+++ src/main/java/frc/robot/Climb.java
@@ -2,17 +2,22 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Climb {
 
     private final VictorSP winchMotor;
     private final VictorSP armMotor;
 
+    private final Solenoid armLockPiston;
+
     private DigitalInput armLimitSwitch;
 
-    public Climb(int winchMotorPort, int armMotorPort, int armLimitSwitchPort) {
+    public Climb(int winchMotorPort, int armMotorPort, int armLockPistonPort, int armLimitSwitchPort) {
         winchMotor = new VictorSP(winchMotorPort);
         armMotor = new VictorSP(armMotorPort);
+        armLockPiston = new Solenoid(armLockPistonPort);
+
         armLimitSwitch = new DigitalInput(armLimitSwitchPort);
     }
 
@@ -22,6 +27,10 @@ public class Climb {
 
     public void moveArms(double power) {
         armMotor.set(power);
+    }
+
+    public void lock(boolean on) {
+        armLockPiston.set(on);
     }
 
     public boolean getArmLimitSwitchValue() {

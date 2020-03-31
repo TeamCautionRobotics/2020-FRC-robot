169a87bdabb410889a914c8b0684b064a20b2a2d
cadenmr <caden.m.r@cox.net>
2020-03-31 00:48:23 -0700

    convert Shoot to instantCommand

--- src/main/java/frc/robot/commands/BallChucker9000Shoot.java
+++ src/main/java/frc/robot/commands/BallChucker9000Shoot.java
@@ -1,40 +1,37 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.BallChucker9000;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 
-public class BallChucker9000Shoot extends CommandBase {
+public class BallChucker9000Shoot extends InstantCommand {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
   private final BallChucker9000 BallChucker9000Subsystem;
+  private boolean forward;
 
   /**
    * Creates a new BallChucker9000Index.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public BallChucker9000Shoot(BallChucker9000 subsystem) {
+  public BallChucker9000Shoot(BallChucker9000 subsystem, boolean forward) {
     BallChucker9000Subsystem = subsystem;
     
-    addRequirements(BallChucker9000Subsystem);
+    this.forward = forward;
   }
 
   @Override
   public void initialize() {
-    BallChucker9000Subsystem.indexerMotorControl(1);
-  }
-
-  @Override
-  public void execute() {
+    if (this.forward) {
+      BallChucker9000Subsystem.indexerMotorControl(0.75);
+    } else {
+      BallChucker9000Subsystem.indexerMotorControl(-0.75);
+    }
   }
 
   @Override
   public void end(boolean interrupted) {
     BallChucker9000Subsystem.indexerMotorControl(0);
-  }
-
-  @Override
-  public boolean isFinished() {
-    return false;
   }
 }

3a271bad7c0a831d0e55c07d5bd35741f5b4c825
cadenmr <epintric@gmail.com>
2021-10-13 12:57:28 -0700

    add comments for limelight methods

--- src/main/java/frc/robot/misc2020/LimelightData.java
+++ src/main/java/frc/robot/misc2020/LimelightData.java
@@ -7,18 +7,22 @@ public class LimelightData {
         NetworkTableInstance.getDefault().getTable("limelight").getEntry(var).setNumber(data);
     }
 
+    // limelight has valid targets (0/1)
     public double getTv() {
         return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
       }
     
+    // limelight target x offset
     public double getTx() {
       return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
     }
   
+    // limelight target y offset
     public double getTy() {
       return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
     }
   
+    // target area of image (0-100%)
     public double getTa() {
       return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
     }

3c2491c045b77e6a22a8c4bd7de85dfb18ed59d9
cadenmr <epintric@gmail.com>
2021-10-15 00:05:32 -0700

    robotcontainer overhaul + base controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,7 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
@@ -17,11 +17,26 @@ import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import frc.robot.commands.ToggleReaper;
+import frc.robot.commands.ArmDown;
+import frc.robot.commands.ArmUp;
+import frc.robot.commands.BallChuckerFlywheelAuto;
+import frc.robot.commands.BallChuckerRotatorAuto;
+import frc.robot.commands.ElevateBalls;
+import frc.robot.commands.LockWinch;
+import frc.robot.commands.RunIndexer;
 import frc.robot.commands.RunReaper;
+import frc.robot.commands.RunWinchDown;
+import frc.robot.commands.RunWinchUp;
 import frc.robot.commands.TankDrive;
 import frc.robot.commands.ToggleShifter;
+import frc.robot.commands.UnlockWinch;
 import frc.robot.misc2020.EnhancedJoystick;
 import frc.robot.misc2020.LimelightData;
+import frc.robot.subsystems.BallChuckerFlywheel;
+import frc.robot.subsystems.BallChuckerIndexer;
+import frc.robot.subsystems.BallChuckerRotator;
+import frc.robot.subsystems.BallTransfer;
+import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.DriveBase;
 import frc.robot.subsystems.Reaper;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -29,6 +44,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -45,8 +61,18 @@ public class RobotContainer {
   XboxController manipulator;
 
   public final DriveBase driveBase;
-
   public final Reaper reaper;
+  public final ClimbSubsystem climb;
+  public final BallChuckerFlywheel ballChuckerFlywheel;
+  public final BallChuckerRotator ballChuckerRotator;
+  public final BallChuckerIndexer ballChuckerIndexer;
+  public final BallTransfer ballTransfer;
+
+  public final LimelightData limelightData;
+
+  public boolean rotatorLocked;
+  public boolean flywheelLocked;
+
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
@@ -59,13 +85,17 @@ public class RobotContainer {
     rightJoystick = new EnhancedJoystick(Constants.RIGHT_JOYSTICK_PORT);
     manipulator = new XboxController(Constants.MANIPULATOR_PORT);
 
-    final LimelightData limelightData;
-
     final VictorSP reaperMotor;
     reaperMotor = new VictorSP(Constants.REAPER_MOTOR_PORT);
     reaperMotor.setInverted(true);
 
-    reaper = new Reaper(reaperMotor, Constants.REAPER_PORT_PISTON_PORT, Constants.REAPER_STARBOARD_PISTON_PORT);
+    limelightData = new LimelightData();
+
+    reaper = new Reaper(
+        reaperMotor,
+        Constants.REAPER_PORT_PISTON_PORT, 
+        Constants.REAPER_STARBOARD_PISTON_PORT);
+
     driveBase = new DriveBase(
         new WPI_TalonSRX(Constants.LEFT_DRIVE_MOTOR_0_DEVICE_NUMBER),
         new WPI_VictorSPX(Constants.LEFT_DRIVE_MOTOR_1_DEVICE_NUMBER),
@@ -78,8 +108,34 @@ public class RobotContainer {
         new Encoder(Constants.LEFT_DRIVE_ENCODER_PORT_A, Constants.LEFT_DRIVE_ENCODER_PORT_B, false, EncodingType.k4X),
         new Encoder(Constants.RIGHT_DRIVE_ENCODER_PORT_A, Constants.RIGHT_DRIVE_ENCODER_PORT_B, true, EncodingType.k4X));
 
+    climb = new ClimbSubsystem(
+        new WPI_VictorSPX(Constants.WINCH_MOTOR_DEVICE_NUMBER), 
+        new WPI_VictorSPX(Constants.ARM_MOTOR_DEVICE_NUMBER), 
+        new Solenoid(Constants.WINCH_LOCK_PISTON_PORT), 
+        new Encoder(Constants.ARM_ENCODER_PORT_A, Constants.ARM_ENCODER_PORT_B), 
+        new DigitalInput(Constants.ARM_LIMIT_SWITCH_PORT));
+
+    ballChuckerFlywheel = new BallChuckerFlywheel(
+        new SpeedControllerGroup(
+          new VictorSP(Constants.LEFT_FLYWHEEL_MOTOR_PORT),
+          new VictorSP(Constants.RIGHT_FLYWHEEL_MOTOR_PORT)),
+        new Encoder(Constants.FLYWHEEL_ENCODER_PORT_A, Constants.FLYWHEEL_ENCODER_PORT_B));
+
+    ballChuckerRotator = new BallChuckerRotator(
+        new WPI_VictorSPX(Constants.ROTATOR_MOTOR_DEVICE_ID),
+        new Encoder(Constants.ROTATOR_ENCODER_PORT_A, Constants.ROTATOR_ENCODER_PORT_B),
+        new DigitalInput(Constants.ROTATOR_LIMIT_SWITCH_PORT));
+
+    ballChuckerIndexer = new BallChuckerIndexer(
+        new WPI_VictorSPX(Constants.INDEXER_MOTOR_DEVICE_ID));
+
+    ballTransfer = new BallTransfer(
+        new VictorSP(Constants.BALL_TRANSFER_MOTOR_PORT));
+    
     driveBase.setDefaultCommand(new TankDrive(driveBase, () -> leftJoystick.getY(), () -> rightJoystick.getY()));
     reaper.setDefaultCommand(new RunReaper(reaper, () -> manipulator.getY(Hand.kRight)));
+    ballChuckerFlywheel.setDefaultCommand(new BallChuckerFlywheelAuto(ballChuckerFlywheel, limelightData, flywheelLocked));
+    ballChuckerRotator.setDefaultCommand(new BallChuckerRotatorAuto(ballChuckerRotator, limelightData, rotatorLocked));
   }
 
   /**
@@ -91,7 +147,20 @@ public class RobotContainer {
   private void configureButtonBindings() {
     new JoystickButton(leftJoystick, 4).toggleWhenPressed(new ToggleShifter(driveBase));
 
+    new JoystickButton(leftJoystick, 1).toggleWhenPressed(new RunReaper(reaper, () -> 0.7));
     new JoystickButton(manipulator, Button.kA.value).toggleWhenPressed(new ToggleReaper(reaper));
+
+    new JoystickButton(leftJoystick, 1).whileHeld(new ElevateBalls(ballTransfer, () -> 0.75));
+
+    new JoystickButton(rightJoystick, 1).whileHeld(new RunIndexer(ballChuckerIndexer, () -> 0.75));
+
+    new JoystickButton(manipulator, Button.kY.value).whenHeld(new ArmUp(climb));
+    new JoystickButton(manipulator, Button.kX.value).whenHeld(new ArmDown(climb));
+
+    new JoystickButton(manipulator, Button.kStart.value).whenPressed(new LockWinch(climb));
+    new JoystickButton(manipulator, Button.kBack.value).whenPressed(new UnlockWinch(climb));
+    new JoystickButton(manipulator, Button.kBumperLeft.value).whenHeld(new RunWinchDown(climb));
+    new JoystickButton(manipulator, Button.kBumperRight.value).whenHeld(new RunWinchUp(climb));
   }
 
   /**
